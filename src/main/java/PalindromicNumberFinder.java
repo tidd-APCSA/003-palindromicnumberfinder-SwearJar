9795a2cfb0266005d20750c01232db0c61aa2539
Sam Schut <samuelschut23@gmail.com>
2020-11-13 22:57:26 +0000

    Finished rest of the methods and changed reverse method comment is in the code.

--- src/main/java/PalindromicNumberFinder.java
+++ src/main/java/PalindromicNumberFinder.java
@@ -6,32 +6,44 @@ public class PalindromicNumberFinder {
         this.num = num;
     }
 
-    // accessor/getter method
+    // // accessor/getter method
     public int getNum(){
         return this.num;
     }
 
-    // this method should find the next palindromic number
+    ////////// this method should find the next palindromic number
     public int searchForPalindromicNum(int num){
-        
+        num++;
+        while(testPalindromicNum(num)!= true){
+          num++;
+        }
+        return num-this.num;
     }
 
-    // this is a helper method for searchForPalindromicNum. It's purpose is to test if a number is actually a palindrome
+    // // this is a helper method for searchForPalindromicNum. It's purpose is to test if a number is actually a palindrome
     public boolean testPalindromicNum(int num){
-
+      String convert = Integer.toString(num);
+      if(convert.equals(reverseNum(num))){
+        return true;
+      }else{
+        return false;
+      }
 
     }
 
 
     // this is a helper method for testPalindromicNum. It should reverse the number and return it.
     public String reverseNum(int num){
-      int reversed = 0;
-      while (num!=0){
-        int value = num % 10;
-        reversed = reversed * 10 + value;
-        num /= 10;
-      }
+      Integer number = num;
+      //String convert = Interger.toString(num); Why doesnt't this wrap not work? It worked in a different enviroment
+      String reversed = "";
+      int length = number.toString().length();
+      for (int i = 0; i < length; i++){
+        reversed = number.toString().charAt(i) + reversed;
+      
+      } 
       return reversed;
+
     }
 
 
